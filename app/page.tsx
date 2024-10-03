21304b576844daac7299fdd2ca516ab834ce112a
Andrew Ulloa <aulloa2017@fau.edu>
2024-10-03 00:16:53 -0500

    Update page.tsx
    Icons border updated

--- app/page.tsx
+++ app/page.tsx
@@ -74,7 +74,7 @@ export default function Home() {
           <div className="text-white text-xs font-supreme-ll">built with love in bogota ❤️</div>
           <div className="flex gap-5 justify-center items-center">
             {['icon-1', 'icon-2', 'icon-3', 'icon-4', 'icon-5'].map((iconName, index) => (
-              <Link key={iconName} href="#" aria-label={`Social media link ${index + 1}`} className="w-7 h-7 p-1 bg-white/10 rounded .shadow shadow-inner border border-white backdrop-blur-[10px] flex items-center justify-center">
+              <Link key={iconName} href="#" aria-label={`Social media link ${index + 1}`} className="w-7 h-7 p-1 bg-white/10 rounded .shadow shadow-inner border border-gradient backdrop-blur-[10px] flex items-center justify-center">
                 <Icon name={iconName} className="w-5 h-5"/>
               </Link>
             ))}
