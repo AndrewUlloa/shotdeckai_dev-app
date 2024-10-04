e6e04fbc1654425e5cdfa0de8f669c7d8deb0d8c
Andrew-243 <aulloa2017@fau.edu>
2024-10-04 17:35:26 -0500

    chore: Update gitignore to include image file patterns and updated bg image.

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import { StoryboardFrameComponent } from "@/components/storyboard-frame";
 export default function Home() {
   return (
     <div 
-      className="w-full h-screen overflow-auto bg-fixed bg-cover bg-center bg-no-repeat shadow-inner flex flex-col justify-between bg-[url('https://imagedelivery.net/qkb4K12RSBaH1a6IAJIhiQ/1dfb1fca-95a0-4769-05fd-e2d376896100/public')]"
+      className="w-full h-screen overflow-auto bg-fixed bg-cover bg-center bg-no-repeat shadow-inner flex flex-col justify-between bg-[url('https://imagedelivery.net/f403c70d-82b9-41c0-95ac-5512ad886500/public')]"
     >
       <div className="w-full max-2xl:px-4 mt-1 sm:px-4 lg:px-8 pt-safe flex flex-col justify-between h-full">
         <header className="w-full flex justify-between items-center py-6">
