b2c39c675ce5531c4dace15aa39ea1d92e8685fd
Andrew-243 <aulloa2017@fau.edu>
2024-10-17 17:41:50 -0500

    fixed spacing on mobile and lg

--- components/prelaunch-signup.tsx
+++ components/prelaunch-signup.tsx
@@ -72,9 +72,9 @@ export default function PrelaunchSignup() {
           className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex items-center justify-center z-50 lg:backdrop-blur-sm"
           onClick={handleOverlayClick}
         >
-          <div className="flex items-center p-1.5 bg-white/60 border-gradient-lg rounded-outer-signup lg:p-2">
+          <div className="m-10 flex items-center p-1.5 bg-white/60 border-gradient-lg rounded-outer-signup lg:p-2">
             <div className="flex flex-col bg-gradient-to-b from-white from-0% via-offwhite via-66% to-bluegray to-90%
-            border-gradient-signup rounded-inner-signup justify-between gap-6 py-6 px-6 lg:gap-8 lg:p-12">
+            border-gradient-signup rounded-inner-signup justify-between gap-6 p-8 lg:gap-8 lg: max-w-full">
               <IconButton className="w-4 h-4" onClick={() => setIsOpen(false)}><X/></IconButton>
               <div className="lg:hidden">
                 <LogoWithText variant="small" />
@@ -83,7 +83,7 @@ export default function PrelaunchSignup() {
                 <LogoWithText variant="large" />
               </div>
               <div className="flex flex-col gap-3 ">
-                <h2 className="text-xl font-eudoxusMedium font-medium tracking-[-0.019rem] text-center lg:text-2xl">Get Early Access</h2>
+                <h2 className="text-2xl font-eudoxusMedium font-medium tracking-[-0.019rem] text-center lg:text-2xl">Get Early Access</h2>
                 <p className="text-sm font-eudoxusLight tracking-[-0.019rem] text-center lg:text-lg">
                   Sign up to be notified when we launch!
                 </p>
@@ -91,7 +91,7 @@ export default function PrelaunchSignup() {
               <form onSubmit={handleSubmit}>
                 <div>
                   <div className="flex flex-row border-2 border-DFE4F2 py-2 pl-4 pr-2 justify-between bg-white rounded-full frame-bg-effects-blur-light">
-                    <Input className="flex flex-grow shadow-none leading-tight font-eudoxusLight placeholder:text-#A1A1A1 lg:min-w-80 text-xl "
+                    <Input className="flex shadow-none leading-tight font-eudoxusLight placeholder:text-#A1A1A1  lg:min-w-80 text-xl"
                       id="email"
                       placeholder="Enter your email"
                       type="email"
